52536977dc6c17dd7cc21bde192a660a95e20f42
AndreVictor60 <andre.victor60@free.fr>
2017-12-12 21:29:59 +0100

    fini

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Partie 5 Exercice 3</title>
+  </head>
+  <body>
+    <?php
+    $month=['janvier','février','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','décembre'];
+    echo $month[5];
+     ?>
+  </body>
+</html>
